8cd57d497d144d232d712074fb4a255c6b00d3b9
Dhruv-Sutail <dhruvsutail338@gmail.com>
2021-12-18 10:16:00 +0530

    update

--- Mojeflix/settings.py
+++ Mojeflix/settings.py
@@ -161,17 +161,3 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/profile/'
 
-AWS_STORAGE_BUCKET_NAME = "cadprojectmojeflix"
-AWS_S3_REGION_NAME = "us-east-2"
-AWS_ACCESS_KEY_ID = "AKIAXIUXNR4I5KFGQ74M"
-AWS_SECRET_ACCESS_KEY = "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"
-
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-STATICFILES_FOLDER = "static"
-MEDIAFILES_FOLDER = "media"
-
-STATICFILES_STORAGE = "custom_storages.StaticFileStorage"
-DEFAULT_FILE_STORAGE = "custom_storages.MediaFileStorage"
